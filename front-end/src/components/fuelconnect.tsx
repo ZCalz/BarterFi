a84c8f8f04a1559fb9538a66f6fdac2b71691366
duhcompuder <gattlingjack@gmail.com>
2023-05-08 22:34:56 -0400

    updated wagmi config

---
+++ front-end/src/components/fuelconnect.tsx
@@ -0,0 +1,67 @@
+
+import { useEffect, useState } from "react";
+import { Wallet } from "fuels";
+import "./App.css";
+// Import the contract factory -- you can find the name in index.ts.
+// You can also do command + space and the com cpiler will suggest the correct name.
+import { ContractsInSwayAbi__factory } from "../contracts";
+
+// The address of the contract deployed the Fuel testnet
+const CONTRACT_ID =
+  "0x2c38161e42ce14abdaa7950c04b13aa340eb67cf196c94b971d5f0175417f4f4";
+
+//the private key from createWallet.js
+const WALLET_SECRET =
+  "0x07960a5124cd6e52b595aed727d640aed2e32cc587a34c09f1b48615378df752";
+
+// Create a Wallet from given secretKey in this case
+// The one we configured at the chainConfig.json
+const wallet = Wallet.fromPrivateKey(
+  WALLET_SECRET,
+  "https://beta-3.fuel.network/graphql"
+);
+
+// Connects out Contract instance to the deployed contract
+// address using the given wallet.
+const contract = ContractsInSwayAbi__factory.connect(CONTRACT_ID, wallet);
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    async function main() {
+      // Executes the counter function to query the current contract state
+      // the `.get()` is read-only, because of this it don't expand coins.
+      const { value } = await contract.functions.count().get();
+      setCounter(Number(value));
+    }
+    main();
+  }, []);
+
+  async function increment() {
+    // a loading state
+    setLoading(true);
+    // Creates a transactions to call the increment function
+    // because it creates a TX and updates the contract state this requires the wallet to have enough coins to cover the costs and also to sign the Transaction
+    try {
+      await contract.functions.increment().txParams({ gasPrice: 1 }).call();
+      const { value } = await contract.functions.count().get();
+      setCounter(Number(value));
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Counter: {counter}</p>
+        <button disabled={loading} onClick={increment}>
+          {loading ? "Incrementing..." : "Increment"}
+        </button>
+      </header>
+    </div>
+  );
+}
+export default App;
