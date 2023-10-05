51e0d08e144b77b45ebafe1e1254835530ea406b
Calvin <csze4107@gmail.com>
2023-10-04 23:15:24 -0400

    clean up

--- front-end/tailwind.config.ts
+++ front-end/tailwind.config.ts
@@ -1,34 +1,10 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
-export default config;
-
 // import type { Config } from "tailwindcss";
-// const withMT = require("@material-tailwind/react/utils/withMT");
 
 // const config: Config = {
 //   content: [
 //     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
 //     "./components/**/*.{js,ts,jsx,tsx,mdx}",
 //     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-//     "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-//     "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
 //   ],
 //   theme: {
 //     extend: {
@@ -41,5 +17,29 @@ export default config;
 //   },
 //   plugins: [],
 // };
+// export default config;
 
-// module.exports = withMT(config);
+import type { Config } from "tailwindcss";
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+const config: Config = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+    },
+  },
+  plugins: [],
+};
+
+module.exports = withMT(config);
